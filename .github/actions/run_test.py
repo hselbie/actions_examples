c36769a438313e4476f8dbd8531d97f9ba93f9a1
Hugo Selbie <hugoselbie@google.com>
2020-11-12 23:56:12 +0000

    ff

--- .github/actions/run_test.py
+++ .github/actions/run_test.py
@@ -1,5 +1,5 @@
 import looker_sdk
 
 print('this is running')
-# sdk = looker_sdk.init31(config_file='test_instance.ini')
-# print(sdk.me())
+sdk = looker_sdk.init31(config_file='test_instance.ini')
+print(sdk.me())
