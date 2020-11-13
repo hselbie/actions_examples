588c57f28244a5bacf46669d6b19239b0e05c37a
Hugo Selbie <hugoselbie@google.com>
2020-11-13 05:50:06 +0000

    adf

--- .github/actions/test_ini_in.py
+++ .github/actions/test_ini_in.py
@@ -5,23 +5,16 @@ import os
 # environment variables
 env_var = os.environ
 env_var = dict(env_var)
-data = {
-    "base_url": env_var.get('BASE_URL'),
-    "client_id": env_var.get('CLIENT_ID'),
-    "client_secret": env_var.get('CLIENT_SECRET'),
-}
 
-template = """
+template = jinja2.Template("""
 [Looker]
 api_version=4.0
 base_url={{ base_url }}
 client_id={{client_id}}
 client_secret={{client_secret}}
 verify_ssl=False
-"""
-
-j2_template = jinja2.Template(template)
-
-with open ('looker.ini', 'w') as ini_file:
-    ini_file.write(j2_template)
-
+""").stream(
+    base_url=env_var.get('BASE_URL'),
+    client_id=env_var.get('CLIENT_ID'),
+    client_secret=env_var.get('CLIENT_SECRET')
+    ).dump('looker.ini')
