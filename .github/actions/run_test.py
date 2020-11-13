21cd322edb1498f70886668553271a329f417510
Hugo Selbie <hugoselbie@google.com>
2020-11-13 05:16:02 +0000

    test

--- .github/actions/run_test.py
+++ .github/actions/run_test.py
@@ -2,10 +2,22 @@ import looker_sdk
 import argparse
 from io import StringIO   
 import configparser as ConfigParser
+import os
+import os 
+import pprint 
+  
+# Get the list of user's 
+# environment variables 
+env_var = os.environ 
+  
+# Print the list of user's 
+# environment variables 
+print("User's Environment variable:") 
+pprint.pprint(dict(env_var), width = 1) 
 
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # parser = argparse.ArgumentParser()
     # parser.add_argument('--id', type=str)
     # parser.add_argument('--secret', type=str)
@@ -25,13 +37,13 @@ if __name__ == "__main__":
     # client_id={args.id}
     # client_secret={args.secret}
     # verify_ssl=False'''
-    ini_file = '.github/actions/test_instance.ini'
-    config = ConfigParser.RawConfigParser(allow_no_value=True)
-    config.read(ini_file)
-    # t = config.get('Looker', 'base_url')
-    # print(t)
+    # ini_file = '.github/actions/test_instance.ini'
+    # config = ConfigParser.RawConfigParser(allow_no_value=True)
+    # config.read(ini_file)
+    # # t = config.get('Looker', 'base_url')
+    # # print(t)
 
 
-    sdk = looker_sdk.init31(config_file=ini_file)
+    # sdk = looker_sdk.init31(config_file=ini_file)
 
-    print(sdk.me())
+    # print(sdk.me())
