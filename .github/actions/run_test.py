fc1f8d379d27804f5c0e2ecc587a73bf8c76a704
Hugo Selbie <hugoselbie@google.com>
2020-11-13 00:30:22 +0000

    DD

--- .github/actions/run_test.py
+++ .github/actions/run_test.py
@@ -1,5 +1,15 @@
 import looker_sdk
+import argparse
 
-print({{ BASE_URL }})
-# sdk = looker_sdk.init31(config_file='test_instance.ini')
-# print(sdk.me())
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--id', type=str)
+    parser.add_argument('--secret', type=str)
+    parser.add_argument('--lookml_param', type=str)
+    args = parser.parse_args()
+
+
+    print(args.id)
+    # sdk = looker_sdk.init31(config_file='test_instance.ini')
+    # print(sdk.me())
