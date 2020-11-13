27c05938041beaba87c92fe9582f56590bca57f5
Hugo Selbie <hugoselbie@google.com>
2020-11-13 05:04:20 +0000

    test

--- .github/actions/run_test.py
+++ .github/actions/run_test.py
@@ -1,33 +1,37 @@
 import looker_sdk
 import argparse
 from io import StringIO   
+import configparser as ConfigParser
 
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--id', type=str)
-    parser.add_argument('--secret', type=str)
-    parser.add_argument('--base', type=str)
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--id', type=str)
+    # parser.add_argument('--secret', type=str)
+    # parser.add_argument('--base', type=str)
+    # # args = parser.parse_args()
 
 
-    print(f'this is my test {args.id}')
     # sdk = looker_sdk.init31(config_file='test_instance.ini')
     # print(sdk.me())
 
     # Importing the StringIO module. 
   
-    string = f'''
-    [Looker]
-    api_version=4.0
-    base_url={args.base}
-    client_id={args.id}
-    client_secret={args.secret}
-    verify_ssl=False'''
-    
-    ini_file = StringIO(string) 
+    # string = f'''
+    # [Looker]
+    # api_version=4.0
+    # base_url={args.base}
+    # client_id={args.id}
+    # client_secret={args.secret}
+    # verify_ssl=False'''
+    ini_file = 'actions_examples/.github/actions/test_instance.ini'
+    config = ConfigParser.RawConfigParser(allow_no_value=True)
+    config.read(ini_file)
+    # t = config.get('Looker', 'base_url')
+    # print(t)
 
-    sdk = looker_sdk.init31(config_file='test_instance.ini')
+
+    sdk = looker_sdk.init31(config_file=ini_file)
 
     print(sdk.me())
