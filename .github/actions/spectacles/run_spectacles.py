505d090561579aa00a665bf312ff0fcc734057d9
hselbie <hselbie@gmail.com>
2020-11-20 23:08:43 +0000

    asdfadf

--- .github/actions/spectacles/run_spectacles.py
+++ .github/actions/spectacles/run_spectacles.py
@@ -11,11 +11,16 @@ CLIENT_SECRET = env_var.get('CLIENT_SECRET')
 sql_response = subprocess.run(
     ['spectacles',
     'sql',
-    '--project','hugo_sandbox' ,
-    '--branch', '${GITHUB_REF##*/}',
-    '--base-url', 'https://profservices.dev.looker.com'
-    '--client-id', CLIENT_ID,
-    '--client-secret', CLIENT_SECRET,
+    '--project',
+    'hugo_sandbox' ,
+    '--branch', 
+    '${GITHUB_REF##*/}',
+    '--base-url', 
+    'https://profservices.dev.looker.com',
+    '--client-id', 
+    CLIENT_ID,
+    '--client-secret', 
+    CLIENT_SECRET,
     '-v'
     ])
 
