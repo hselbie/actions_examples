419406822f753e4cbf535b0359e3695b04b034d7
hselbie <hselbie@gmail.com>
2020-11-20 05:34:56 +0000

    spectacles first step

---
+++ .github/actions/spectacles/configyml.py
@@ -0,0 +1,16 @@
+import jinja2
+import os
+
+env_var = os.environ
+env_var = dict(env_var)
+
+template = jinja2.Template("""
+base_url={{ base_url }}
+client_id={{client_id}}
+client_secret={{client_secret}}
+verify_ssl=False
+""").stream(
+    base_url=env_var.get('BASE_URL'),
+    client_id=env_var.get('CLIENT_ID'),
+    client_secret=env_var.get('CLIENT_SECRET')
+    ).dump('config.yml')
